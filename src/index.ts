9a0d1707206c50c89bb7d1718516e38e4da8d49a
bharatayasa <bharatayasa40@gmail.com>
2025-02-24 13:58:27 +0800

    fic change port to contabo

--- src/index.ts
+++ src/index.ts
@@ -13,9 +13,12 @@ const app = express();
 app.use(express.json());
 app.use(routes); 
 
-const port = process.env.PORT ? parseInt(process.env.PORT, 10) : 3002;
-const host = process.env.HOST || '0.0.0.0';
+const port = `${process.env.PORT}`;
+const host = `${process.env.HOST}`;
 
-app.listen(port, host, () => {
+console.log("ini adalah port:", port);
+console.log("ini adalah host:", `${host}`);
+
+app.listen(3000, host, () => {
     console.log(`Server is running on http://${host}:${port}`);
 });
