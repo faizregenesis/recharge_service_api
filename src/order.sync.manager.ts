2c9bc1c40132d808c19bc9c48f5e412fa4d9ff86
bharatayasa <bharatayasa40@gmail.com>
2025-03-09 21:02:38 +0800

    update users data

--- src/order.sync.manager.ts
+++ src/order.sync.manager.ts
@@ -1,11 +1,13 @@
 import {
-    consumeUserData
+    consumeUserData, 
+    consumeUsersDataUpdate
 } from './routes/comsume_user/consumer.pod.user';
 
 
 const runFunctionsInOrder = async () => {
     try {
         await consumeUserData()
+        await consumeUsersDataUpdate()
 
         console.log('All functions executed successfully in order');
     } catch (error) {
