cda38957d360276fb3b752f3a2fdf892fd71114d
bharatayasa <bharatayasa40@gmail.com>
2025-05-22 11:14:15 +0800

    create self dev sound

--- src/routes/consume_self_dev_sound_bounce/consumer.self.dev.sounds.ts
+++ src/routes/consume_self_dev_sound_bounce/consumer.self.dev.sounds.ts
@@ -2,8 +2,8 @@ import amqp from 'amqplib';
 import dotenv from  "dotenv"
 import prisma from '../../../prisma/prisma';
 import {
-    bounceCreateSelfDevToAdmin, 
-    bounceUpdateSelfDevToAdmin
+    bounceCreateSelfDevSoundToAdmin, 
+    bounceUpdateSelfDevSoundToAdmin
 } from './publish.to..queue';
 
 dotenv.config(); 
@@ -11,6 +11,7 @@ dotenv.config();
 const upsertSelfDevSoundDataExchange  = process.env.UPSERT_SELF_DEVELOPMENT_SOUND_EXCHANGE;
 const createSelfDevSoundGroupExchangeName  = `${process.env.CREATE_SELF_DEV_BY_GROUP_EXCHANGE}`;
 const updateSelfDevSoundGroupExchangeName  = `${process.env.UPDATE_SELF_DEV_BY_GROUP_EXCHANGE}`;
+const deleteSelfDevSoundDataExchange  = process.env.DELETE_SELF_DEVELOPMENT_SOUND_EXCHANGE;
 
 const connectionUrl = process.env.RABBITMQ_URL;
 
@@ -103,20 +104,20 @@ const consumeCreateSelfDevgGroup = async () => {
                 const podData = await prisma.pod.findMany({
                     where: {
                         fk_group_id: {
-                            in: data. group_ids
+                            in: data.group_ids
                         }
                     }
                 })
                 const podIds = podData.map(id => id.id)
-                // console.log("ini adalah data pod id yang sesuai dengan group: ", podIds);
+                console.log("ini adalah data pod id yang sesuai dengan group: ", podIds);
 
                 // 2. dapatkan semua data self dev yang sesuai dengan pod
                 const selfDevData = await prisma.self_development2.findMany({
                     where: {
                         fk_pod_id: {
                             in: podIds
-                        }, 
-                        self_development_name: data.self_development_name
+                        }
+                        // self_development_name: data.self_development_name
                     }
                 })
                 const selfDevId = selfDevData.map(idSelfDev => idSelfDev.id)
@@ -127,21 +128,23 @@ const consumeCreateSelfDevgGroup = async () => {
                 const insertPromises = selfDevId.map(id => {
                     return prisma.self_development_sound2.create({
                         data: {
-                        self_development_id: id,
-                        sound_code: data.soundData.sound_code, 
-                        title: data.soundData.title, 
-                        duration: data.soundData.duration, 
-                        description: data.soundData.description, 
-                        sound_path: data.soundData.sound_path, 
-                        file_path: data.soundData.file_path, 
-                        caption: data.soundData.caption,
-                        created_date: now,
-                        updated_date: now
+                            self_development_id: id,
+                            sound_code: data.soundData.sound_code, 
+                            title: data.soundData.title, 
+                            duration: data.soundData.duration, 
+                            description: data.soundData.description, 
+                            sound_path: data.soundData.sound_path, 
+                            file_path: data.soundData.file_path, 
+                            caption: data.soundData.caption,
+                            created_date: now,
+                            updated_date: now
                         }
                     });
                 });
 
                 const insertedRecords = await Promise.all(insertPromises);
+                console.log("insertedRecords", insertedRecords);
+
                 const formattingBounceMessage = insertedRecords.map(sound => ({
                     id: sound.id, 
                     self_development_id: sound.self_development_id, 
@@ -152,7 +155,7 @@ const consumeCreateSelfDevgGroup = async () => {
                     sound_path: sound.sound_path, 
                     file_path: sound.file_path
                 }))
-                // console.log("Inserted records:", formattingBounceMessage);
+                console.log("Inserted records:", formattingBounceMessage);
                 
                 const message = {
                     data: formattingBounceMessage, 
@@ -160,9 +163,9 @@ const consumeCreateSelfDevgGroup = async () => {
                     podIds: podIds, 
                     data0: formattingBounceMessage[0], 
                     self_development_name: data.self_development_name
-                }
+                } 
 
-                await bounceCreateSelfDevToAdmin(message)
+                await bounceCreateSelfDevSoundToAdmin(message)
 
                 channel.ack(msg);
             } catch (error: any) {
@@ -276,7 +279,7 @@ const consumeUpdateSelfDevgGroup = async () => {
                 }
                 // console.log("ini adalah message yang akan dikirim ke pod dan juga ke admin: ", message);
 
-                await bounceUpdateSelfDevToAdmin(message);
+                await bounceUpdateSelfDevSoundToAdmin(message);
 
                 channel.ack(msg);
             } catch (error: any) {
@@ -289,8 +292,59 @@ const consumeUpdateSelfDevgGroup = async () => {
     }
 };
 
+const deleteSelfDevSoundData = async () => {
+    try {
+        const connection = await amqp.connect(`${connectionUrl}`);
+        const channel = await connection.createChannel();
+
+        await channel.assertExchange(`${deleteSelfDevSoundDataExchange}`, 'fanout', { durable: true });
+
+        const { queue } = await channel.assertQueue('', { exclusive: true });
+        await channel.bindQueue(queue, `${deleteSelfDevSoundDataExchange}`, '');
+        channel.prefetch(1);
+
+        console.log(`\x1b[32mPod is waiting for messages on queue (delete self def sound data): ${queue}\x1b[0m`);
+
+        channel.consume(queue, async (msg) => {
+            if (!msg) return;
+
+            try {
+                const messageContent = msg.content.toString();
+                console.log("ini adalah message yang diterima delete self dev sound: ", messageContent);
+
+                const existSelfDevSoundData = await prisma.self_development_sound2.findUnique({
+                    where: {
+                        id: `${messageContent}`
+                    }
+                })
+
+                // console.log("ini adalah self dev yang tersedia: ", existSelfDevSoundData);
+
+                if (existSelfDevSoundData) {
+                    await prisma.self_development_sound2.delete({
+                        where: {
+                            id: `${messageContent}`
+                        }
+                    })
+                    console.log("\x1b[32m✅ Self dev sound data deleted in database\x1b[0m");
+                } else {
+                    console.log("self dev sound data is not for this pod, skip deleting data");
+                }
+
+                channel.ack(msg);
+            } catch (error: any) {
+                console.error('\x1b[31m❌ Error processing message:', error.message, '\x1b[0m');
+                channel.nack(msg, false, true);
+            }
+        });
+    } catch (error) {
+        console.error('\x1b[31m❌ Error initializing consumer:', error, '\x1b[0m');
+    }
+};
+
 export {
     consumeCreateSelfDevgGroup, 
     consumeInsertSelfDevSoundData, 
-    consumeUpdateSelfDevgGroup
+    consumeUpdateSelfDevgGroup, 
+    deleteSelfDevSoundData
 }
