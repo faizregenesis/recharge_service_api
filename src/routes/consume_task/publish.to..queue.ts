6041ead96744928d2aadcb7d57a882371a473c45
bharatayasa <bharatayasa40@gmail.com>
2025-06-09 13:26:34 +0800

    add task data

--- src/routes/consume_task/publish.to..queue.ts
+++ src/routes/consume_task/publish.to..queue.ts
@@ -3,7 +3,7 @@ const dotenv = require('dotenv');
 dotenv.config();
 
 const connectionUrl = process.env.RABBITMQ_URL;
-const bounceNodeData  = `${process.env.BOUNCE_TASK_DATA}`;
+const bounceTaskData  = `${process.env.BOUNCE_TASK_DATA}`;
 const bounceDeleteNodeData  = `${process.env.BOUNCE_DELETE_TASK_DATA}`;
 
 const bounceTaskDataToAdmin = async (message: any) => {
@@ -11,10 +11,10 @@ const bounceTaskDataToAdmin = async (message: any) => {
         const connection = await amqp.connect(`${connectionUrl}`);
         const channel = await connection.createChannel();
 
-        await channel.assertExchange(bounceNodeData, 'fanout', { durable: true });
+        await channel.assertExchange(bounceTaskData, 'fanout', { durable: true });
 
         const messageString = typeof message === 'string' ? message : JSON.stringify(message);
-        channel.publish(bounceNodeData, '', Buffer.from(messageString));
+        channel.publish(bounceTaskData, '', Buffer.from(messageString));
         console.log(`Create task data spread to pods and admin bounce: ${message}`);
 
         await channel.close();
